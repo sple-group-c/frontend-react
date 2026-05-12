4dfe646885d155ea9cbc14cd19b7994646a0d5e9
Haekal Kalipaksi <hkalipaksi@proton.me>
2026-05-12 16:59:07 +0700

    Merge branch 'main' of https://github.com/sple-group-c/frontend-react

--- src/taskManagement/components/TaskmanagementTable.jsx
+++ src/taskManagement/components/TaskmanagementTable.jsx
@@ -1,24 +1,19 @@
 
-import React from 'react'
-import { Link } from "react-router";
+import React from 'react';
+import { Link, useNavigate } from "react-router";
 import { useParams } from "@/commons/hooks/useParams"
-
-import { useAuth } from '@/commons/auth'
-import { Button } from '@/commons/components';
-
+import { useAuth } from '@/commons/auth';
+import { Button, Modal, Spinner } from '@/commons/components';
 import * as Layouts from "@/commons/layouts";
-
-const invalidCard = ({ taskData,
-		 
-
+const TaskmanagementTable = ({ listTaskmanagement,
 	}) => {
   const { checkPermission } = useAuth();
   
   return (
-    <Layouts.ListComponentCardLayout
-      items={[taskData]}
-  	
-  	itemsAttrs={[
+  <>
+    <Layouts.ListComponentTableLayout
+  	  items={[listTaskmanagement]}
+  	  itemsAttrs={[
           {
             id: "title",
             condition: "",
@@ -41,20 +36,9 @@ const invalidCard = ({ taskData,
             editable: false
           }
   ]}
-      itemsEvents={(Item) => [
-        <Link to=''>
-          <Button
-            id="_vRENIEBiEfG_EbYrkoLnzw"
-            size="sm"
-            variant=
-                        "secondary"
-          >
-            Edit
-          </Button>
-        </Link>
-  	]}
-    />
-  )	
+  	/>
+  </>
+  )
 };
 
-export default invalidCard;
+export default TaskmanagementTable;
--- src/taskManagement/routes.jsx
+++ src/taskManagement/routes.jsx
@@ -2,22 +2,16 @@
 import React from 'react';
 import TaskList from './containers/TaskList'
 import AddTask from './containers/AddTask'
-import Edit from './containers/Edit'
 
 const taskManagementRoutes = [
 { 
-    path: "/task",
+    path: "/taskmanagement",
     element: <TaskList />,
   }	
 ,
 { 
     path: "/task/tambah",
     element: <AddTask />,
-  }	
-,
-{ 
-    path: "",
-    element: <Edit />,
   }	
 
 ]
